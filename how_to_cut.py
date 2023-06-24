b58dd65425ac99d7ea54b39eab905776fbb22129
Patryk Pilecki <ppilecki@icloud.com>
2023-06-24 01:23:33 +0100

    material cutting app

---
+++ how_to_cut.py
@@ -0,0 +1,45 @@
+def check_cut_placement(dimensions_of_the_material, dimensions_of_the_cuts):
+    sorted_cuts = sorted(dimensions_of_the_cuts.items(), key=lambda x: x[1][0] * x[1][1], reverse=True)
+    material_width, material_height = dimensions_of_the_material
+    remaining_material = [[0, 0, material_width, material_height]]
+    cut_placements = {}
+
+    for cut_name, cut_dimensions in sorted_cuts:
+        for i, (x, y, w, h) in enumerate(remaining_material):
+            if cut_dimensions[0] <= w and cut_dimensions[1] <= h:
+                cut_placements[cut_name] = [(x, y), (x + cut_dimensions[0], y), (x + cut_dimensions[0], y + cut_dimensions[1]), (x, y + cut_dimensions[1])]
+                if cut_dimensions[0] == w and cut_dimensions[1] == h:
+                    del remaining_material[i]
+                else:
+                    if cut_dimensions[0] == w:
+                        remaining_material[i] = [x, y + cut_dimensions[1], w, h - cut_dimensions[1]]
+                    elif cut_dimensions[1] == h:
+                        remaining_material[i] = [x + cut_dimensions[0], y, w - cut_dimensions[0], h]
+                    else:
+                        remaining_material[i] = [x + cut_dimensions[0], y, w - cut_dimensions[0], cut_dimensions[1]]
+                        remaining_material.append([x, y + cut_dimensions[1], w, h - cut_dimensions[1]])
+                break
+        else:
+            return "Material too small"
+
+    output = "Optimal Placement:\n"
+    for cut_name, coordinates in cut_placements.items():
+        output += f"{cut_name}:\n"
+        for coordinate in coordinates:
+            output += f"        {coordinate}\n"
+
+    return output
+
+
+# Example usage
+dimensions_of_the_material = [105, 68]
+dimensions_of_the_cuts = {
+    "A": [52.5, 41],
+    "B": [41, 17],
+    "C": [105, 7],
+    "D": [52.5, 41],
+    "E": [41, 17]
+}
+
+result = check_cut_placement(dimensions_of_the_material, dimensions_of_the_cuts)
+print(result)
